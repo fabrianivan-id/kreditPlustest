6e8539dcbcfbc9d20627900510d91b45978214fd
fabrianivan <fabrian.ivan@gmail.com>
2023-03-09 19:05:25 +0700

    User fixed

--- controller/customer_test.go
+++ controller/customer_test.go
@@ -1,1 +1,253 @@
 package controller
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"kreditPlus-test/config"
+	util "kreditPlus-test/library"
+	"kreditPlus-test/model"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	setup()
+	os.Exit(m.Run())
+}
+
+func setup() {
+	// -- Create Connection
+	config := config.GetConfig()
+	db := util.MysqlDatabaseConnTest(config)
+
+	// -- Clean DB Data
+	db.Migrator().DropTable(&models.Transaction{})
+	db.Migrator().DropTable(&models.CartDetails{})
+	db.Migrator().DropTable(&models.Cart{})
+	db.Migrator().DropTable(&models.Checkout{})
+	db.Migrator().DropTable(&models.Stock{})
+	db.Migrator().DropTable(&models.RecipeIngredients{})
+	db.Migrator().DropTable(&models.RecipeCategories{})
+	db.Migrator().DropTable(&models.Ingredient{})
+	db.Migrator().DropTable(&models.Category{})
+	db.Migrator().DropTable(&models.Recipe{})
+	db.Migrator().DropTable(&models.User{})
+	db.AutoMigrate(&models.User{})
+
+	// -- Dummy Data
+	var newUser models.User
+	newUser.Name = "Testing Name"
+	newUser.Email = "testingmailt@mail.com"
+	newUser.Password = "generate111"
+	newUser.Address = "jl. barat lau no 1"
+	newUser.Gender = "laki"
+	newUser.Role = "admin"
+
+	// -- Dummy Data with Model
+	userModel := models.NewUserModel(db)
+	_, err := userModel.Register(newUser)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func TestValidRegisterUserController(t *testing.T) {
+	// -- Create Connection and Controller
+	config := config.GetConfig()
+	db := util.MysqlDatabaseConnTest(config)
+	userModel := models.NewUserModel(db)
+	authController := NewAuthController(userModel)
+
+	// -- Declare Route
+	e := echo.New()
+	e.POST("/api/register", authController.RegisterUserController)
+
+	// -- Input
+	reqBodyPost, _ := json.Marshal(map[string]interface{}{
+		"name":     "Buddy",
+		"email":    "buddy@gmail.com",
+		"password": "generate111",
+		"address":  "jl barat daya no 5",
+		"gender":   "laki",
+		"role":     "admin",
+	})
+
+	// -- Setting Controller
+	req := httptest.NewRequest(echo.POST, "/api/register", bytes.NewBuffer(reqBodyPost))
+	req.Header.Set("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	e.ServeHTTP(res, req)
+
+	// -- Declare Response and Convert to JSON
+	type Response struct {
+		Success bool   `json:"success"`
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	}
+	var response Response
+
+	json.Unmarshal(res.Body.Bytes(), &response)
+
+	assert.Equal(t, true, response.Success)
+	assert.Equal(t, 200, res.Code)
+	assert.Equal(t, "Success Register Account", response.Message)
+}
+
+func TestInvalidRegisterUserControllerEmailNull(t *testing.T) {
+	// -- Create Connection and Controller
+	config := config.GetConfig()
+	db := util.MysqlDatabaseConnTest(config)
+	userModel := models.NewUserModel(db)
+	authController := NewAuthController(userModel)
+
+	// -- Declare Route
+	e := echo.New()
+	e.POST("/api/register", authController.RegisterUserController)
+
+	// -- Input
+	reqBodyPost, _ := json.Marshal(map[string]interface{}{
+		"name":     "Buddy",
+		"password": "generate111",
+		"address":  "jl barat daya no 5",
+		"gender":   "laki",
+		"role":     "customer",
+	})
+
+	// -- Setting Controller
+	req := httptest.NewRequest(echo.POST, "/api/register", bytes.NewBuffer(reqBodyPost))
+	req.Header.Set("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	e.ServeHTTP(res, req)
+
+	// -- Declare Response and Convert to JSON
+	type Response struct {
+		Success bool   `json:"success"`
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	}
+	var response Response
+
+	json.Unmarshal(res.Body.Bytes(), &response)
+
+	assert.Equal(t, false, response.Success)
+	assert.Equal(t, 400, res.Code)
+	assert.Equal(t, "Bad Request", response.Message)
+}
+
+func TestInvalidRegisterUserControllerAllNull(t *testing.T) {
+	// -- Create Connection and Controller
+	config := config.GetConfig()
+	db := util.MysqlDatabaseConnTest(config)
+	userModel := models.NewUserModel(db)
+	authController := NewAuthController(userModel)
+
+	// -- Declare Route
+	e := echo.New()
+	e.POST("/api/register", authController.RegisterUserController)
+
+	// -- Input
+	reqBodyPost, _ := json.Marshal(map[string]interface{}{})
+
+	// -- Setting Controller
+	req := httptest.NewRequest(echo.POST, "/api/register", bytes.NewBuffer(reqBodyPost))
+	req.Header.Set("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	e.ServeHTTP(res, req)
+
+	// -- Declare Response and Convert to JSON
+	type Response struct {
+		Success bool   `json:"success"`
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	}
+	var response Response
+
+	json.Unmarshal(res.Body.Bytes(), &response)
+
+	assert.Equal(t, false, response.Success)
+	assert.Equal(t, 500, res.Code)
+	assert.Equal(t, "Internal Server Error", response.Message)
+}
+
+func TestValidLoginUserController(t *testing.T) {
+	// -- Create Connection and Controller
+	config := config.GetConfig()
+	db := util.MysqlDatabaseConnTest(config)
+	userModel := models.NewUserModel(db)
+	authController := NewAuthController(userModel)
+
+	// -- Declare Route
+	e := echo.New()
+	e.POST("/api/login", authController.LoginUserController)
+
+	// -- Input
+	reqBodyPost, _ := json.Marshal(map[string]interface{}{
+		"email":    "buddy@gmail.com",
+		"password": "generate111",
+	})
+
+	// -- Setting Controller
+	req := httptest.NewRequest(echo.POST, "/api/login", bytes.NewBuffer(reqBodyPost))
+	req.Header.Set("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	e.ServeHTTP(res, req)
+
+	// -- Declare Response and Convert to JSON
+	type Response struct {
+		Success bool   `json:"success"`
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+		Token   string `json:"token"`
+	}
+	var response Response
+
+	json.Unmarshal(res.Body.Bytes(), &response)
+
+	assert.Equal(t, true, response.Success)
+	assert.Equal(t, 200, res.Code)
+	assert.Equal(t, "Success Login", response.Message)
+	assert.NotEqual(t, "", response.Token)
+}
+
+func TestInvalidLoginUserController(t *testing.T) {
+	// -- Create Connection and Controller
+	config := config.GetConfig()
+	db := util.MysqlDatabaseConnTest(config)
+	userModel := models.NewUserModel(db)
+	authController := NewAuthController(userModel)
+
+	// -- Declare Route
+	e := echo.New()
+	e.POST("/api/login", authController.LoginUserController)
+
+	// -- Input
+	reqBodyPost, _ := json.Marshal(map[string]interface{}{
+		"email":    "buddy@gmail.com",
+		"password": "generate112",
+	})
+
+	// -- Setting Controller
+	req := httptest.NewRequest(echo.POST, "/api/login", bytes.NewBuffer(reqBodyPost))
+	req.Header.Set("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	e.ServeHTTP(res, req)
+
+	// -- Declare Response and Convert to JSON
+	type Response struct {
+		Success bool   `json:"success"`
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	}
+	var response Response
+
+	json.Unmarshal(res.Body.Bytes(), &response)
+
+	assert.Equal(t, false, response.Success)
+	assert.Equal(t, 400, res.Code)
+	assert.Equal(t, "Bad Request", response.Message)
+}
--- router/router.go
+++ router/router.go
@@ -4,18 +4,18 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"kreditPlus-test/constant"
+	"kreditPlus-test/controller"
 )
 
 func Route(
 	e *echo.Echo,
 	customerController *controller.CustomerController,
-	loanReqController *controller.LoanController,
 ) {
 	// ------------------------------------------------------------------
 	// Auth Login & Register
 	// ------------------------------------------------------------------
-	e.POST("/api/register", customerController.RegisterCustomerController)
-	e.POST("/api/login", customerController.LogincustomerController)
+	e.POST("/api/register", customerController.RegisterUserController)
+	e.POST("/api/login", customerController.LoginUserController)
 
 	// ------------------------------------------------------------------
 	// Auth JWT
@@ -25,15 +25,15 @@ func Route(
 	// ------------------------------------------------------------------
 	// Layanan Loan Request
 	// ------------------------------------------------------------------
-	e.GET("/api/loanrequest", controller.GetAllRecipeController, jwtMiddleware)
+	//e.GET("/api/loanrequest", controller.GetAllRecipeController, jwtMiddleware)
 
 	// ------------------------------------------------------------------
 	// Loan Limit
 	// ------------------------------------------------------------------
-	e.GET("/api/categories", categoryController.GetAllCategoryController, jwtMiddleware)
-	e.POST("/api/categories", customerControllerontroller.InsertCategoryController, jwtMiddleware)
-	e.GET("/api/categories/:id", categoryController.GetCategoryController, jwtMiddleware)
-	e.PUT("/api/categories/:id", categoryController.EditCategoryController, jwtMiddleware)
-	e.DELETE("/api/categories/:id", categoryController.DeleteCategoryController, jwtMiddleware)
+	//e.GET("/api/categories", categoryController.GetAllCategoryController, jwtMiddleware)
+	//e.POST("/api/categories", customerControllerontroller.InsertCategoryController, jwtMiddleware)
+	//e.GET("/api/categories/:id", categoryController.GetCategoryController, jwtMiddleware)
+	//e.PUT("/api/categories/:id", categoryController.EditCategoryController, jwtMiddleware)
+	//e.DELETE("/api/categories/:id", categoryController.DeleteCategoryController, jwtMiddleware)
 
 }
